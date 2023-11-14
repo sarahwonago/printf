4f3e416b07d2e18d334ce64a25e1a7029d8b0dfd
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-14 16:11:16 +0300

    Update print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -1,35 +1,20 @@
 #include "main.h"
 
 /**
- * print_binary - converts int to binary
+ * print_binary - converts unsigned int to binary
  * @args: argument
- * Return: integer
+ * Return: number of characters printed
  */
-
 int print_binary(va_list args)
 {
-    int f = 0;
     int count = 0;
-    int i, a = 1, b;
     unsigned int num = va_arg(args, unsigned int);
-    unsigned int p;
 
-    for(i = 0; i < 32; i++)
+    for (int i = 31; i >= 0; i--)
     {
-        p = ((a << (32 - i)) & num);
-        if(p >> (31 - i))
-            f = 1;
-        if(f)
-        {
-            b = p >> (31 - i);
-            _putchar(b + 48);
-            count++;
-        }
-    }
-    if(count == 0)
-    {
+        _putchar((num & (1 << i)) ? '1' : '0');
         count++;
-        _putchar('0');
     }
-    return(count);
+
+    return (count);
 }

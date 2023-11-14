d19a042fd1496baf3296e36937a50c555f22e518
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-14 16:32:59 +0300

    Update print_HEX.c

--- print_HEX.c
+++ print_HEX.c
@@ -1,29 +1,39 @@
 #include "main.h"
 
 /**
- * print_hexadecimal - converts to hexadecimal
+ * print_HEX - converts to HEXADECIMAL
  * @val: argument
  * Return: counter
  */
 
-int print_HEX(va_list val)
-{
-    int counter = 0;
-    unsigned int num = va_arg(val);
+ int print_HEX(va_list val)
+ {
+     int i, counter = 0;
+     int *array;
+     unsigned int num = va_arg(val, unsigned int);
+     unsigned int temp = num;
 
-    int array[8];  // Assuming a 32-bit integer, so 8 hex digits
+     while(num / 16 != 0)
+     {
+         num = num / 16;
+         counter++;
+     }
+     counter++;
+     array = malloc(sizeof(int) * counter);
+     for(i = 0; i < counter; i++)
+     {
+         array[i] = temp % 16;
+         temp = temp / 16;
+     }
+     for(i = counter - 1; i >= 0; i--)
+     {
+        if(array[i] > 9)
+        {
+            array[i] = array[i] + 7;
+        }
+        _putchar(array[i] + '0');
+     }
+     free(array);
+     return(counter);
+ }
 
-    while (num != 0)
-    {
-        array[counter] = num % 16;
-        num /= 16;
-        counter++;
-    }
-
-    for (int i = counter - 1; i >= 0; i--)
-    {
-        _putchar((array[i] < 10) ? (array[i] + '0') : (array[i] - 10 + 'A'));
-    }
-
-    return counter;
-}

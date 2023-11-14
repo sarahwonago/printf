9730b4216c5659551bf8695d79d20073a22c7158
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-14 14:42:36 +0300

    Update print_strlen.c

--- print_strlen.c
+++ print_strlen.c
@@ -3,28 +3,25 @@
 /**
  * print_strlen - returns the length of a string
  * @s: pointer to a string
- * Return: 1
+ * Return: length of the string
  */
+int print_strlen(char *s)
+{
+    int i;
 
- int print_strlen(char *s)
- {
-     int i;
+    for (i = 0; s[i] != '\0'; i++);
+    return i;
+}
 
-     for(i = 0; s[i] != 0; i++);
-     return (i);
- }
-
- /**
+/**
  * print_strlenc - returns the length of a constant string
  * @str: pointer to a string
- * Return: 1
+ * Return: length of the string
  */
+int print_strlenc(const char *str)
+{
+    int i;
 
- int print_strlenc(const char *str)
- {
-     int i;
-
-     for(i = 0; str[i] != 0; i++);
-     return (i);
- }
-
+    for (i = 0; str[i] != '\0'; i++);
+    return i;
+}

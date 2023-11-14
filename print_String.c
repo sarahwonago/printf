842007809726d3abb516ab301096d762c0caabc8
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-14 16:41:28 +0300

    Create print_String.c

---
+++ print_String.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_String - print string and others
+ * @val: argument
+ * Return: the length of the string
+ */
+
+ int print_String(va_list val)
+ {
+     char *str;
+     int i, value;
+     int length = 0;
+
+     str = va_arg(val, char *);
+     if(str == NULL)
+     {
+         str = "(null)";
+     }
+     for(i = 0; str[i] != '\0'; i++)
+     {
+         if(str[i] < 32 || str[i] >= 127)
+         {
+             _putchar('\\');
+             _putchar('x');
+             length = length + 2;
+             value = str[i];
+             if(value < 16)
+             {
+                 _putchar('0');
+                 length++;
+             }
+             length = length + print_HEX_valparam(value);
+         }
+         else
+         {
+             _putchar(str[i]);
+             length++;
+         }
+     }
+     return(length);
+ }
+

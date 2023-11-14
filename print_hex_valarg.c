2d1df8932089f0fc62799dfbf3cbce127a22f630
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-14 16:48:03 +0300

    print_hex_valarg.c

---
+++ print_hex_valarg.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_hex_valarg - converts to hexadecimal
+ * @val: argument
+ * Return: counter
+ */
+
+ int print_hex_valarg(unsigned long int num)
+ {
+     long int i, counter = 0;
+     long int *array;
+     unsigned long int temp = num;
+
+     while(num / 16 != 0)
+     {
+         num = num / 16;
+         counter++;
+     }
+     counter++;
+     array = malloc(sizeof(long int) * counter);
+     for(i = 0; i < counter; i++)
+     {
+         array[i] = temp % 16;
+         temp = temp / 16;
+     }
+     for(i = counter - 1; i >= 0; i--)
+     {
+        if(array[i] > 9)
+        {
+            array[i] = array[i] + 39;
+        }
+        _putchar(array[i] + '0');
+     }
+     free(array);
+     return(counter);
+ }

4b7123fc0c26fcc060a2e0deef31d6f81ea80000
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-14 16:32:16 +0300

    Update print_hexadecimal.c

--- print_hexadecimal.c
+++ print_hexadecimal.c
@@ -6,24 +6,33 @@
  * Return: counter
  */
 
-int print_hexadecimal(va_list val)
-{
-    int counter = 0;
-    unsigned int num = va_arg(val);
+ int print_hexadecimal(va_list val)
+ {
+     int i, counter = 0;
+     int *array;
+     unsigned int num = va_arg(val, unsigned int);
+     unsigned int temp = num;
 
-    int array[8];  // Assuming a 32-bit integer, so 8 hex digits
-
-    while (num != 0)
-    {
-        array[counter] = num % 16;
-        num /= 16;
-        counter++;
-    }
-
-    for (int i = counter - 1; i >= 0; i--)
-    {
-        _putchar((array[i] < 10) ? (array[i] + '0') : (array[i] - 10 + 'a'));
-    }
-
-    return counter;
-}
+     while(num / 16 != 0)
+     {
+         num = num / 16;
+         counter++;
+     }
+     counter++;
+     array = malloc(sizeof(int) * counter);
+     for(i = 0; i < counter; i++)
+     {
+         array[i] = temp % 16;
+         temp = temp / 16;
+     }
+     for(i = counter - 1; i >= 0; i--)
+     {
+        if(array[i] > 9)
+        {
+            array[i] = array[i] + 39;
+        }
+        _putchar(array[i] + '0');
+     }
+     free(array);
+     return(counter);
+ }

a2e5a48b6558957a5f8e304593815920093f320e
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-14 15:41:14 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -7,11 +7,13 @@
 int _printf(const char * const format, ...)
 {
     match p[] = {
-        {"%c", print_c},
-        {"%s", print_s}, 
-        {"%%", print_percentage}
-        {"%i", print_integer},
-        {"%d", print_integer_dec}
+        {"%s", print_s}, {"%c", print_c},
+        {"%%", print_percentage},
+        {"%i", print_integer}, {"%d", print_integer_dec}, {"%r", print_s_reverse},
+        {"%R", print_rot13}, {"%b", print_binary},
+        {"%u", print_unsigned_int},
+        {"%o", print_octal}, {"%x", print_hexadecimal}, {"%X", print_HEX},
+        {"%S", print_String}, {"%p", print_address}
     };
 
     va_list args;

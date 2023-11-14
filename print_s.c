67f6636aa8dde773e1066a2f8629873771d746a9
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2023-11-14 14:36:19 +0300

    Update print_s.c

--- print_s.c
+++ print_s.c
@@ -5,31 +5,22 @@
  * @args: argument
  * Return: the length of the string
  */
+int print_s(va_list args)
+{
+    char *str;
+    int i, length;
 
- int print_s(va_list args)
- {
-     char *str;
-     int i;
-     int length;
+    str = va_arg(args, char *);
 
-     str = va_arg(args, char *);
-     if(str == NULL)
-     {
-         str = "(null)";
-         length = print_strlen(str);
-         for(i = 0; i < length; i++)
-         {
-             _putchar(str[i]);
-         }
-         return (length);
-     }
-     else
-     {
-         length = print_strlen(str);
-         for(i = 0; i < length; i++)
-         {
-             _putchar(str[i]);
-         }
-         return (length);
-     }
- }
+    if (str == NULL)
+        str = "(null)";
+
+    length = print_strlen(str);
+
+    for (i = 0; i < length; i++)
+    {
+        _putchar(str[i]);
+    }
+
+    return length;
+}
